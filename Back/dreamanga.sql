2585599ba381a2f34f5522143f81cb3461d8a01e
lies93 <hocini.lies@gmail.com>
2023-11-04 15:13:30 +0100

    add example to databases

--- Back/dreamanga.sql
+++ Back/dreamanga.sql
@@ -2,56 +2,81 @@ DROP DATABASE IF EXISTS DreamangaDataBase;
 
 CREATE DATABASE DreamangaDataBase;
 
+USE DreamangaDataBase;
+
 CREATE TABLE IF NOT EXISTS DreamangaDataBase.User (
-  idUser INT AUTO_INCREMENT,
+  idUser INT AUTO_INCREMENT PRIMARY KEY,
   pseudo VARCHAR(20),
   birthday DATETIME NULL,
   email VARCHAR(45),
-  password VARCHAR(255),
-  PRIMARY KEY (idUser)
+  password VARCHAR(255)
 );
-
--- INSERT INTO DreamangaDataBase.User VALUES (NULL,'Morley','1992-08-13 00:04:37','mjimenez2@so-net.ne.jp','dsdsd');
--- INSERT INTO DreamangaDataBase.User VALUES (NULL,'Brnaby','1991-03-09 06:32:04','bwhales0@last.fm','oV4}G|KQ6V');
--- INSERT INTO DreamangaDataBase.User VALUES (NULL,'Brion','1996-05-22 22:55:11','bbatalini4@google.com.au','dY8$wV|_@O!KV8f');
--- INSERT INTO DreamangaDataBase.User VALUES (NULL,'Marve','1998-01-06 03:52:59','mdorton5@theguardian.com','rU0+V/?J');
+  
 
 CREATE TABLE IF NOT EXISTS DreamangaDataBase.Post (
-  idPost INT NOT NULL AUTO_INCREMENT,
+  idPost INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(45) NOT NULL,
   description VARCHAR(200) NOT NULL,
-  datePublication DATETIME NULL,
-  idUser INT NOT NULL,
-  PRIMARY KEY (idPost),
-  INDEX idUser_idx (idUser),
-  CONSTRAINT idUser
-    FOREIGN KEY (idUser)
-    REFERENCES DreamangaDataBase.User(idUser)
+  datePublication DATE NULL,
+  idUser INT,
+  CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES User (idUser)
 );
 
+--  Exemple de requete pour Inserer un User
+INSERT INTO User (idUser,pseudo, birthday, email, password) VALUES (NULL,'NomUtilisateur', '1990-10-30', 'utilisateur@example.com', 'MotDePasseSecret');
+-- INSERT INTO User (idUser,pseudo, birthday, email, password) VALUES (NULL,'NomUtilisar', '1990-10-30', 'utilisateur@example.com', 'MotDePasseSecr');
+
+-- Exemple de request pour insert un post
+
+INSERT INTO Post(idPost, title, description, datePublication,idUser) VALUES (NULL,"shonen","incroyable aventureeeee","2012-01-06",1);
+-- INSERT INTO Post(idPost, title, description, datePublication,idUser) VALUES (NULL,"Haha","incroyable aventureeeee","2013-01-06",2);
+
+
+-- CREATE TABLE IF NOT EXISTS DreamangaDataBase.Commentary (
+--   idCommentary INT AUTO_INCREMENT PRIMARY KEY,
+--   subject VARCHAR(200) NULL,
+--   datePublication DATETIME NULL,
+--   idUser INT,
+--   idPost INT,
+--   role VARCHAR(15) NULL,
+--   isConnected TINYINT NULL,
+--   INDEX idPost_idx (idPost),
+--   INDEX idUser_idx (idUser),
+--     CONSTRAINT fk_idPost FOREIGN KEY (idPost) REFERENCES User (idPost),
+--     CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES User (idUser),
+--     FOREIGN KEY (idPost) REFERENCES DreamangaDataBase.Post (idPost),
+--     FOREIGN KEY (idUser) REFERENCES DreamangaDataBase.User (idUser)
+-- );
+
 CREATE TABLE IF NOT EXISTS DreamangaDataBase.Commentary (
-  idCommentary INT NOT NULL AUTO_INCREMENT,
+  idCommentary INT AUTO_INCREMENT PRIMARY KEY,
   subject VARCHAR(200) NULL,
   datePublication DATETIME NULL,
-  idUser INT NULL,
-  idPost INT NULL,
+  idUser INT,
+  idPost INT,
   role VARCHAR(15) NULL,
   isConnected TINYINT NULL,
-  PRIMARY KEY (idCommentary),
   INDEX idPost_idx (idPost),
   INDEX idUser_idx (idUser),
-  CONSTRAINT idPost
-    FOREIGN KEY (idPost)
-    REFERENCES DreamangaDataBase.Post (idPost),
-  CONSTRAINT userId
-    FOREIGN KEY (idUser)
-    REFERENCES DreamangaDataBase.User (idUser)
+  CONSTRAINT fk_idPost FOREIGN KEY (idPost) REFERENCES Post (idPost),
+  CONSTRAINT fk_idUser_Commentary FOREIGN KEY (idUser) REFERENCES User (idUser)
 );
+
+
+-- INSERT INTO Commentary(idCommentary, subject, datePublication, idUser,idPost,role,isConnected,) VALUES( NULL, "les shonen c'est pas top","2013-01-06 14:00:00",1,1,"lecteur",1)
+
+INSERT INTO Commentary (subject, datePublication, idUser, idPost, role, isConnected) VALUES ('Sujet du commentaire',NULL, 1, 1, 'lecteur', 1);
+
 
 CREATE TABLE IF NOT EXISTS DreamangaDataBase.Rubrique (
   ID INT AUTO_INCREMENT PRIMARY KEY,
   NomRubrique VARCHAR(255) NOT NULL,
   Description VARCHAR(1000),
   TypeManga VARCHAR(255) NOT NULL,
-  idUser INT NOT NULL
+  idUser INT
 );
+
+INSERT INTO Rubrique (NomRubrique, Description, TypeManga, idUser) VALUES ('Shonen', 'Mangas pour jeunes garçons', 'Shonen', 1);
+
+-- INSERT INTO Rubrique (NomRubrique, Description, TypeManga, idUser)
+-- VALUES ('Shojo', 'Mangas pour jeunes filles', 'Shojo', 2);
