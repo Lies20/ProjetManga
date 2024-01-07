5361a5ea73675871f252c6ad4ab53f6abc8b61a3
lies93 <hocini.lies@gmail.com>
2024-01-07 15:04:34 +0100

    reset server config

--- Back/server.js
+++ Back/server.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const multer = require('multer');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 const port = 3006;
 
 app.use(cors({
   credentials: true,
-  origin: '*', // Remplace cela par le domaine de ton frontend
+  origin: 'https://projet-manga-ph95.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}))
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://projet-manga-ph95.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
