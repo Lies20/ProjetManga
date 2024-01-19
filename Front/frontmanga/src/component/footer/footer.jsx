424a471a8823bcf80c8bf7d0ecae5960393408f5
lies93 <hocini.lies@gmail.com>
2024-01-19 09:20:06 +0100

    footer

--- Front/frontmanga/src/component/footer/footer.jsx
+++ Front/frontmanga/src/component/footer/footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="footer-content">
         <div className="social-icons">
-          {/* Icônes de réseaux sociaux (à remplacer par vos propres icônes) */}
           <a href="#" className="social-icon"><i className="fab fa-facebook"></i></a>
           <a href="#" className="social-icon"><i className="fab fa-twitter"></i></a>
           <a href="#" className="social-icon"><i className="fab fa-instagram"></i></a>
