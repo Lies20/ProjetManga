02fb555d37ca105ddf014b529474b9f388a19b7a
lies93 <hocini.lies@gmail.com>
2024-02-21 15:49:55 +0100

    Resolve import config.js

--- Front/frontmanga/vite.config.js
+++ Front/frontmanga/vite.config.js
@@ -6,8 +6,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // Ajoutez des alias pour les packages de Material-UI
       '@mui/icons-material': '@mui/icons-material/esm',
       '@mui/material': '@mui/material/esm',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@mui/icons-material'],
     },
   },
 })
