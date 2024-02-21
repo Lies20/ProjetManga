25e63e567d48c6bff884a2d82d7926448890742d
lies93 <hocini.lies@gmail.com>
2024-02-21 15:22:39 +0100

    add icon

--- Front/frontmanga/src/component/post/post.jsx
+++ Front/frontmanga/src/component/post/post.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useUser } from '../../contexte/UserContext';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './post.css';
 
 const LatestPosts = () => {
