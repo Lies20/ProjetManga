026186bcbff85f56fd87efbec539cd9aae51d936
lies93 <hocini.lies@gmail.com>
2024-02-21 15:54:04 +0100

    rollback vite.config.js

--- Front/frontmanga/vite.config.js
+++ Front/frontmanga/vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Ajoutez des alias pour les packages de Material-UI
-      '@mui/icons-material': '@mui/icons-material/esm',
-      '@mui/material': '@mui/material/esm',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['@mui/icons-material'],
-    },
-  },
 })
