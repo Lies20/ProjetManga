effe5a409346b3a0f585e72be60695c73c4f08a4
lies hocini <92815639+Lies20@users.noreply.github.com>
2025-06-23 21:10:38 +0000

    add .env

--- Front/frontmanga/src/component/postDetail/postDetail.jsx
+++ Front/frontmanga/src/component/postDetail/postDetail.jsx
@@ -29,7 +29,7 @@ const PostDetail = () => {
   });
   const [showPostActions, setShowPostActions] = useState(false);
 
-  const API_URL = 'http://localhost:3006' || 'https://projetmanga-backend.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://projetmanga-backend.onrender.com';
 
   const handleToggleActions = () => {
     setShowPostActions(!showPostActions);
